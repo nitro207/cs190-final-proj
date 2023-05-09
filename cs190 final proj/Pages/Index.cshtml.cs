7302d3687ae77ae8d43ad230c605a0bc84fa8b17
Anthony Scott <Anthony Scott@DESKTOP-CS2I9Q9>
2023-05-08 21:37:50 -0700

    Add project files.

---
+++ cs190 final proj/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cs190_final_proj.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
