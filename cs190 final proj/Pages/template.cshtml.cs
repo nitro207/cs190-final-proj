77fa64db700b41e90a596b23f762304f3a27d3d4
nitro207 <128526025+nitro207@users.noreply.github.com>
2023-05-11 17:09:40 -0700

    update 2

---
+++ cs190 final proj/Pages/template.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cs190_final_proj.Pages
+{
+    public class TemplateModel : PageModel
+    {
+        private readonly ILogger<TemplateModel> _logger;
+
+        public TemplateModel(ILogger<TemplateModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
